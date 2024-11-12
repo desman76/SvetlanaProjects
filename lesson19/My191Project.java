4e0c99830f31f540f0123f90b69e6b51da40ecab
desman76 <desman76@mail.ru>
2024-11-12 21:17:25 +0300

    добавлен lesson4

---
+++ lesson19/My191Project.java
@@ -0,0 +1,73 @@
+package lesson19;
+
+public class My191Project {
+    public static void main(String[] args) {
+        xInMain();
+        xInMethod1();
+        xInMethod2();
+        xInMethod3();
+        xInMethod4();
+        xInMethod5();
+        xInMethod6();
+        xInMethod7();
+        xInMethod8();
+        xInMethod9();
+        xInMethod10();
+    }
+
+    public static void xInMain() {
+        int x = 1;
+        System.out.println("x in main: " + x);
+    }
+
+    public static void xInMethod1() {
+        int x = 2;
+        System.out.println("x in method 1: " + x);
+    }
+
+    public static void xInMethod2() {
+        int x = 3;
+        System.out.println("x in method 2: " + x);
+    }
+
+    public static void xInMethod3() {
+        int x = 4;
+        System.out.println("x in method 3: " + x);
+    }
+
+    public static void xInMethod4() {
+        int x = 5;
+        System.out.println("x in method 4: " + x);
+    }
+
+    public static void xInMethod5() {
+        int x = 6;
+        System.out.println("x in method 5: " + x);
+    }
+
+    public static void xInMethod6() {
+        int x = 7;
+        System.out.println("x in method 6: " + x);
+    }
+
+    public static void xInMethod7() {
+        int x = 8;
+        System.out.println("x in method 7: " + x);
+    }
+
+    public static void xInMethod8() {
+        int x = 9;
+        System.out.println("x in method 8: " + x);
+    }
+
+    public static void xInMethod9() {
+        int x = 10;
+        System.out.println("x in method 9: " + x);
+    }
+
+    public static void xInMethod10() {
+        int x = 11;
+        System.out.println("x in method 10: " + x);
+    }
+    }
+
---
+++ lesson19/My192Project.java
@@ -0,0 +1,10 @@
+package lesson19;
+
+public class My192Project {
+    public static void main(String[] args) {
+        int[] x = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < x.length; i++) {
+            System.out.println("x[" + i + "] = " + x[i]);
+        }
+    }
+}
